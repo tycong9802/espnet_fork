a9f8a7769c679ef76c4fd601420b9d66a0619406
Zhenqing Hu <hustc12@icloud.com>
2023-08-02 19:56:37 +0000

    Extracted the padding procedure to a function.
    Signed-off-by: Zhenqing Hu <hustc12@icloud.com>

---
+++ utils/common_utils.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn.functional as F
+
+def padding_audio_vanilla(speech, target_length):
+    audio_input = torch.zeros([1, target_length])
+    for i in range(len(speech[0])-1):
+        audio_input[0,i] = speech[0,i]
+    speech = audio_input
+    return speech, torch.tensor([speech.size(1)])
+
+def padding_audio(input_tensor, desired_shape):
+    # padding = desired_shape[1] - input_tensor.size(1)
+    padding_needed = [max(desired_shape[i] - input_tensor[i].shape, 0) for i in range(len(desired_shape))]
+    padded_tensor = F.pad(input_tensor, pad=padding_needed)
+    # padded_tensor = F.pad(input_tensor, (0, 0, 0, padding, 0, 0))
+    return padded_tensor, torch.tensor([padded_tensor.size(1)])
+
+
+def padding_feats(input_tensor, desired_shape):
+    padding = desired_shape[1] - input_tensor.size(1)
+    padded_tensor = F.pad(input_tensor, (0, 0, 0, padding, 0, 0))
+    return padded_tensor, torch.tensor([padded_tensor.size(1)])
